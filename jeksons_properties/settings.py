03d9a4dfe062f4e3c18359019ab526ff39d14998
crystal <crystalokd@gmail.com>
2022-06-21 10:45:52 +0100

    ..

--- jeksons_properties/settings.py
+++ jeksons_properties/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'jeksons_properties/static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 django_heroku.settings(locals())
 
 # Default primary key field type
